142561228714e6e7011db5cabb8901c270eaee1b
Muradha <mhdrizki098@gmail.com>
2022-11-24 00:02:24 +0700

    form barang dengan fitur  tambah, ubah , hapus dan batal isi form

---
+++ Aplikasi kasir/FormBarang.Designer.vb
@@ -0,0 +1,435 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormBarang
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CmbKategori = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TxtCari = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TxtNama = New System.Windows.Forms.TextBox()
+        Me.TxtKode = New System.Windows.Forms.TextBox()
+        Me.BtnBatal = New System.Windows.Forms.Button()
+        Me.BtnSimpan = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TransaksiPembelianToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BtnHapus = New System.Windows.Forms.Button()
+        Me.TransaksiPenjualanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MinumanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OwnerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KasirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogoutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoginToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BtnUbah = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.TransaksiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.BtnSatuan = New System.Windows.Forms.Button()
+        Me.BtnKategori = New System.Windows.Forms.Button()
+        Me.RbKode = New System.Windows.Forms.RadioButton()
+        Me.RbNama = New System.Windows.Forms.RadioButton()
+        Me.CmbSatuan = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TxtStock = New System.Windows.Forms.TextBox()
+        Me.kode = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CmbKategori
+        '
+        Me.CmbKategori.FormattingEnabled = True
+        Me.CmbKategori.Location = New System.Drawing.Point(356, 32)
+        Me.CmbKategori.Name = "CmbKategori"
+        Me.CmbKategori.Size = New System.Drawing.Size(121, 21)
+        Me.CmbKategori.TabIndex = 30
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(308, 35)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 13)
+        Me.Label6.TabIndex = 28
+        Me.Label6.Text = "Kategori"
+        '
+        'TxtCari
+        '
+        Me.TxtCari.Location = New System.Drawing.Point(337, 210)
+        Me.TxtCari.Name = "TxtCari"
+        Me.TxtCari.Size = New System.Drawing.Size(136, 20)
+        Me.TxtCari.TabIndex = 26
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(32, 212)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(94, 13)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Text = "Cari Berdasarkan :"
+        '
+        'TxtNama
+        '
+        Me.TxtNama.Location = New System.Drawing.Point(135, 72)
+        Me.TxtNama.Name = "TxtNama"
+        Me.TxtNama.Size = New System.Drawing.Size(119, 20)
+        Me.TxtNama.TabIndex = 22
+        '
+        'TxtKode
+        '
+        Me.TxtKode.Location = New System.Drawing.Point(135, 34)
+        Me.TxtKode.Name = "TxtKode"
+        Me.TxtKode.Size = New System.Drawing.Size(119, 20)
+        Me.TxtKode.TabIndex = 21
+        '
+        'BtnBatal
+        '
+        Me.BtnBatal.Location = New System.Drawing.Point(316, 19)
+        Me.BtnBatal.Name = "BtnBatal"
+        Me.BtnBatal.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBatal.TabIndex = 18
+        Me.BtnBatal.Text = "&Batal"
+        Me.BtnBatal.UseVisualStyleBackColor = True
+        '
+        'BtnSimpan
+        '
+        Me.BtnSimpan.Location = New System.Drawing.Point(40, 19)
+        Me.BtnSimpan.Name = "BtnSimpan"
+        Me.BtnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSimpan.TabIndex = 15
+        Me.BtnSimpan.Text = "&Simpan"
+        Me.BtnSimpan.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Kode Barang / Barcode"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(92, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Nama"
+        '
+        'TransaksiPembelianToolStripMenuItem
+        '
+        Me.TransaksiPembelianToolStripMenuItem.Name = "TransaksiPembelianToolStripMenuItem"
+        Me.TransaksiPembelianToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.TransaksiPembelianToolStripMenuItem.Text = "Transaksi Pembelian"
+        '
+        'BtnHapus
+        '
+        Me.BtnHapus.Location = New System.Drawing.Point(224, 19)
+        Me.BtnHapus.Name = "BtnHapus"
+        Me.BtnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.BtnHapus.TabIndex = 17
+        Me.BtnHapus.Text = "&Hapus"
+        Me.BtnHapus.UseVisualStyleBackColor = True
+        '
+        'TransaksiPenjualanToolStripMenuItem
+        '
+        Me.TransaksiPenjualanToolStripMenuItem.Name = "TransaksiPenjualanToolStripMenuItem"
+        Me.TransaksiPenjualanToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.TransaksiPenjualanToolStripMenuItem.Text = "Transaksi Penjualan"
+        '
+        'MinumanToolStripMenuItem
+        '
+        Me.MinumanToolStripMenuItem.Name = "MinumanToolStripMenuItem"
+        Me.MinumanToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.MinumanToolStripMenuItem.Text = "Minuman"
+        '
+        'OwnerToolStripMenuItem
+        '
+        Me.OwnerToolStripMenuItem.Name = "OwnerToolStripMenuItem"
+        Me.OwnerToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.OwnerToolStripMenuItem.Text = "Owner"
+        '
+        'KasirToolStripMenuItem
+        '
+        Me.KasirToolStripMenuItem.Name = "KasirToolStripMenuItem"
+        Me.KasirToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.KasirToolStripMenuItem.Text = "Kasir"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KasirToolStripMenuItem, Me.OwnerToolStripMenuItem, Me.MinumanToolStripMenuItem})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(85, 20)
+        Me.ToolStripMenuItem1.Text = "Data Master"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'LogoutToolStripMenuItem
+        '
+        Me.LogoutToolStripMenuItem.Name = "LogoutToolStripMenuItem"
+        Me.LogoutToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.LogoutToolStripMenuItem.Text = "Logout"
+        '
+        'LoginToolStripMenuItem
+        '
+        Me.LoginToolStripMenuItem.Name = "LoginToolStripMenuItem"
+        Me.LoginToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.LoginToolStripMenuItem.Text = "Login"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoginToolStripMenuItem, Me.LogoutToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(54, 20)
+        Me.ToolStripMenuItem2.Text = "Profile"
+        '
+        'BtnUbah
+        '
+        Me.BtnUbah.Location = New System.Drawing.Point(132, 19)
+        Me.BtnUbah.Name = "BtnUbah"
+        Me.BtnUbah.Size = New System.Drawing.Size(75, 23)
+        Me.BtnUbah.TabIndex = 16
+        Me.BtnUbah.Text = "&Ubah"
+        Me.BtnUbah.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(23, 237)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(708, 189)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2, Me.ToolStripMenuItem1, Me.TransaksiToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(754, 24)
+        Me.MenuStrip1.TabIndex = 14
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'TransaksiToolStripMenuItem
+        '
+        Me.TransaksiToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TransaksiPenjualanToolStripMenuItem, Me.TransaksiPembelianToolStripMenuItem})
+        Me.TransaksiToolStripMenuItem.Name = "TransaksiToolStripMenuItem"
+        Me.TransaksiToolStripMenuItem.Size = New System.Drawing.Size(74, 20)
+        Me.TransaksiToolStripMenuItem.Text = "Transaksi"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.BtnSatuan)
+        Me.Panel1.Controls.Add(Me.BtnKategori)
+        Me.Panel1.Controls.Add(Me.RbKode)
+        Me.Panel1.Controls.Add(Me.RbNama)
+        Me.Panel1.Controls.Add(Me.CmbSatuan)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.CmbKategori)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.TxtStock)
+        Me.Panel1.Controls.Add(Me.TxtCari)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.TxtNama)
+        Me.Panel1.Controls.Add(Me.TxtKode)
+        Me.Panel1.Controls.Add(Me.kode)
+        Me.Panel1.Controls.Add(Me.DataGridView1)
+        Me.Panel1.Controls.Add(Me.MenuStrip1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.GroupBox2)
+        Me.Panel1.Location = New System.Drawing.Point(12, 7)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(754, 557)
+        Me.Panel1.TabIndex = 17
+        '
+        'BtnSatuan
+        '
+        Me.BtnSatuan.Location = New System.Drawing.Point(483, 68)
+        Me.BtnSatuan.Name = "BtnSatuan"
+        Me.BtnSatuan.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSatuan.TabIndex = 38
+        Me.BtnSatuan.Text = "Cari"
+        Me.BtnSatuan.UseVisualStyleBackColor = True
+        '
+        'BtnKategori
+        '
+        Me.BtnKategori.Location = New System.Drawing.Point(483, 32)
+        Me.BtnKategori.Name = "BtnKategori"
+        Me.BtnKategori.Size = New System.Drawing.Size(75, 23)
+        Me.BtnKategori.TabIndex = 37
+        Me.BtnKategori.Text = "Cari"
+        Me.BtnKategori.UseVisualStyleBackColor = True
+        '
+        'RbKode
+        '
+        Me.RbKode.AutoSize = True
+        Me.RbKode.Location = New System.Drawing.Point(237, 212)
+        Me.RbKode.Name = "RbKode"
+        Me.RbKode.Size = New System.Drawing.Size(87, 17)
+        Me.RbKode.TabIndex = 34
+        Me.RbKode.TabStop = True
+        Me.RbKode.Text = "Kode Barang"
+        Me.RbKode.UseVisualStyleBackColor = True
+        '
+        'RbNama
+        '
+        Me.RbNama.AutoSize = True
+        Me.RbNama.Location = New System.Drawing.Point(140, 212)
+        Me.RbNama.Name = "RbNama"
+        Me.RbNama.Size = New System.Drawing.Size(90, 17)
+        Me.RbNama.TabIndex = 33
+        Me.RbNama.TabStop = True
+        Me.RbNama.Text = "Nama Barang"
+        Me.RbNama.UseVisualStyleBackColor = True
+        '
+        'CmbSatuan
+        '
+        Me.CmbSatuan.FormattingEnabled = True
+        Me.CmbSatuan.Location = New System.Drawing.Point(356, 69)
+        Me.CmbSatuan.Name = "CmbSatuan"
+        Me.CmbSatuan.Size = New System.Drawing.Size(121, 21)
+        Me.CmbSatuan.TabIndex = 32
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(313, 72)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 13)
+        Me.Label7.TabIndex = 31
+        Me.Label7.Text = "Satuan"
+        '
+        'TxtStock
+        '
+        Me.TxtStock.Location = New System.Drawing.Point(356, 103)
+        Me.TxtStock.Name = "TxtStock"
+        Me.TxtStock.Size = New System.Drawing.Size(121, 20)
+        Me.TxtStock.TabIndex = 27
+        '
+        'kode
+        '
+        Me.kode.AutoSize = True
+        Me.kode.Location = New System.Drawing.Point(311, 106)
+        Me.kode.Name = "kode"
+        Me.kode.Size = New System.Drawing.Size(35, 13)
+        Me.kode.TabIndex = 0
+        Me.kode.Text = "Stock"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BtnUbah)
+        Me.GroupBox1.Controls.Add(Me.BtnSimpan)
+        Me.GroupBox1.Controls.Add(Me.BtnHapus)
+        Me.GroupBox1.Controls.Add(Me.BtnBatal)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 136)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(415, 52)
+        Me.GroupBox1.TabIndex = 35
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Proses"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 194)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(731, 243)
+        Me.GroupBox2.TabIndex = 36
+        Me.GroupBox2.TabStop = False
+        '
+        'FormBarang
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 576)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "FormBarang"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form Barang"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CmbKategori As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TxtCari As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TxtNama As TextBox
+    Friend WithEvents TxtKode As TextBox
+    Friend WithEvents BtnBatal As Button
+    Friend WithEvents BtnSimpan As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TransaksiPembelianToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BtnHapus As Button
+    Friend WithEvents TransaksiPenjualanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MinumanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OwnerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KasirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogoutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoginToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents BtnUbah As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents TransaksiToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents CmbSatuan As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TxtStock As TextBox
+    Friend WithEvents kode As Label
+    Friend WithEvents BtnSatuan As Button
+    Friend WithEvents BtnKategori As Button
+    Friend WithEvents RbKode As RadioButton
+    Friend WithEvents RbNama As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ Aplikasi kasir/FormBarang.vb
@@ -0,0 +1,204 @@
+﻿Imports Oracle.ManagedDataAccess.Client
+Public Class FormBarang
+    Dim conn As New OracleConnection(oradb)
+    Dim cmd As New OracleCommand
+    Dim rd As OracleDataReader
+    Dim da As OracleDataAdapter
+    Dim cb As OracleCommandBuilder
+    Dim ds As DataSet
+
+    Sub Bersih()
+        TxtKode.Text = ""
+        TxtNama.Text = ""
+        CmbKategori.Text = ""
+        CmbSatuan.Text = ""
+        TxtStock.Text = ""
+
+        TxtKode.Enabled = True
+        BtnUbah.Enabled = False
+        BtnHapus.Enabled = False
+        BtnSimpan.Enabled = True
+    End Sub
+    Sub TampilGrid()
+        da = New OracleDataAdapter("SELECT * FROM barang", conn)
+        ds = New DataSet
+        ds.Clear()
+        da.Fill(ds, "BARANG")
+        DataGridView1.DataSource = (ds.Tables("BARANG"))
+        DataGridView1.ReadOnly = True
+    End Sub
+
+    Sub TampilKategori()
+        cmd = New OracleCommand("SELECT * FROM kategori", conn)
+        rd = cmd.ExecuteReader
+        Do While rd.Read
+            CmbKategori.Items.Add(rd.Item(0))
+        Loop
+    End Sub
+
+    Sub TampilSatuan()
+        cmd = New OracleCommand("SELECT * FROM satuan", conn)
+        rd = cmd.ExecuteReader
+        Do While rd.Read
+            CmbSatuan.Items.Add(rd.Item(0))
+        Loop
+    End Sub
+
+    Private Sub FormBarang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conn.Open()
+        TxtKode.Focus()
+        TampilGrid()
+        TampilKategori()
+        TampilSatuan()
+        conn.Close()
+    End Sub
+
+    Private Sub TxtKode_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtKode.KeyPress
+        conn.Open()
+        If e.KeyChar = Chr(13) Then
+            cmd = New OracleCommand("SELECT * FROM barang WHERE kode_barang='" & TxtKode.Text & "'", conn)
+            rd = cmd.ExecuteReader
+            rd.Read()
+            If rd.HasRows = True Then
+                MsgBox("Data Ditemukan", vbInformation + vbOKOnly, "Pesan")
+                TxtKode.Text = rd.Item("KODE_BARANG")
+                TxtNama.Text = rd.Item("NAMA_BARANG")
+                CmbKategori.Text = rd.Item("KODE_KATEGORI")
+                CmbSatuan.Text = rd.Item("SATUAN_BARANG")
+                TxtStock.Text = rd.Item("STOCK_BARANG")
+
+                TxtKode.Enabled = False
+                BtnSimpan.Enabled = False
+                BtnHapus.Enabled = True
+                BtnUbah.Enabled = True
+                BtnBatal.Focus()
+            Else
+                TxtNama.Focus()
+            End If
+        End If
+        conn.Close()
+    End Sub
+
+    Private Sub TxtNama_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtNama.KeyPress
+        If e.KeyChar = Chr(13) Then
+            CmbKategori.Focus()
+        End If
+    End Sub
+
+    Private Sub CmbKategori_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CmbKategori.SelectedIndexChanged
+        CmbSatuan.Focus()
+    End Sub
+
+    Private Sub CmbSatuan_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CmbSatuan.SelectedIndexChanged
+        TxtStock.Focus()
+    End Sub
+
+
+    Private Sub TxtStock_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtStock.KeyPress
+        If e.KeyChar = Chr(13) Then
+            BtnSimpan.Focus()
+        Else
+            BtnUbah.Focus()
+        End If
+        If Not ((e.KeyChar >= "0" And e.KeyChar <= "9") Or e.KeyChar = vbBack) Then e.Handled() = True
+    End Sub
+
+    Private Sub BtnKategori_Click(sender As Object, e As EventArgs) Handles BtnKategori.Click
+        FormKategori.Show()
+    End Sub
+
+    Private Sub BtnSatuan_Click(sender As Object, e As EventArgs) Handles BtnSatuan.Click
+        FormSatuan.Show()
+    End Sub
+
+    Private Sub BtnSimpan_Click(sender As Object, e As EventArgs) Handles BtnSimpan.Click
+        conn.Open()
+
+        If TxtKode.Text = "" Or TxtNama.Text = "" Then
+            MsgBox("Kode / Nama Barang masih ada yang kosong")
+            Exit Sub
+        Else
+            cmd = New OracleCommand("SELECT * FROM barang WHERE kode_barang='" & TxtKode.Text & "'", conn)
+            rd = cmd.ExecuteReader
+            rd.Read()
+
+            If Not rd.HasRows Then
+                Dim queryTambah As String = "INSERT INTO barang(kode_barang,nama_barang,kode_kategori,satuan_barang,stock_barang) values('" & TxtKode.Text & "','" & TxtNama.Text & "','" & CmbKategori.Text & "','" & CmbSatuan.Text & "','" & Val(TxtStock.Text) & "')"
+                cmd = New OracleCommand(queryTambah, conn)
+                cmd.ExecuteNonQuery()
+                Bersih()
+                TampilGrid()
+                TxtKode.Focus()
+            Else
+                MsgBox("Kode barang telah ada sebelumnya", vbInformation + vbOKOnly, "Pesan")
+                TampilGrid()
+            End If
+        End If
+        conn.Close()
+    End Sub
+
+    Private Sub BtnBatal_Click(sender As Object, e As EventArgs) Handles BtnBatal.Click
+        Bersih()
+        TampilGrid()
+        TxtKode.Focus()
+    End Sub
+
+    Private Sub BtnUbah_Click(sender As Object, e As EventArgs) Handles BtnUbah.Click
+        conn.Open()
+
+        If TxtKode.Text = "" Then
+            MsgBox("Kode masih kosong")
+            BtnBatal.Focus()
+            Exit Sub
+        Else
+            cmd = New OracleCommand("UPDATE barang SET nama_barang='" & TxtNama.Text & "',kode_kategori='" & CmbKategori.Text & "',satuan_barang='" & CmbSatuan.Text & "',stock_barang='" & Val(TxtStock.Text) & "' WHERE kode_barang='" & TxtKode.Text & "'", conn)
+            cmd.ExecuteNonQuery()
+            Bersih()
+            TampilGrid()
+            TxtKode.Focus()
+        End If
+        conn.Close()
+    End Sub
+
+    Private Sub BtnHapus_Click(sender As Object, e As EventArgs) Handles BtnHapus.Click
+        conn.Open()
+        If TxtKode.Text = "" Then
+            MsgBox("Kode masih kosong")
+            TxtNama.Focus()
+            Exit Sub
+        Else
+            If MessageBox.Show("Yakin akan dihapus...?", "Konfirmasi", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+
+                cmd = New OracleCommand("DELETE FROM barang WHERE kode_barang='" & TxtKode.Text & "'", conn)
+                cmd.ExecuteNonQuery()
+                Bersih()
+                TampilGrid()
+                TxtKode.Focus()
+            Else
+                Bersih()
+                TampilGrid()
+                TxtKode.Focus()
+
+            End If
+        End If
+        conn.Close()
+    End Sub
+
+    Private Sub TxtCari_TextChanged(sender As Object, e As EventArgs) Handles TxtCari.TextChanged
+        If RbNama.Checked = True Then
+            da = New OracleDataAdapter("SELECT * FROM barang WHERE nama_barang like '%" & TxtCari.Text & "%' ORDER BY nama_barang ASC", conn)
+            ds = New DataSet
+            ds.Clear()
+            da.Fill(ds, "BARANG")
+            DataGridView1.DataSource = (ds.Tables("BARANG"))
+            DataGridView1.ReadOnly = True
+        ElseIf RbKode.Checked = True Then
+            da = New OracleDataAdapter("SELECT * FROM barang WHERE kode_barang like '%" & TxtCari.Text & "%' ORDER BY kode_barang ASC", conn)
+            ds = New DataSet
+            ds.Clear()
+            da.Fill(ds, "BARANG")
+            DataGridView1.DataSource = (ds.Tables("BARANG"))
+            DataGridView1.ReadOnly = True
+        End If
+    End Sub
+End Class
